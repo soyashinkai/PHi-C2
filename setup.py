0db6431551dccd00868b0ec23afc6642291060a7
soyashinkai <39551850+soyashinkai@users.noreply.github.com>
2021-10-15 14:48:45 +0900

    Upload setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="phic",
+    version="2.0.0",
+    author="Soya SHINKAI",
+    author_email="soya.shinkai@riken.jp",
+    license="GPL-3.0",
+    description="Polymer dynamics deciphered from Hi-C data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["biophysics", "Hi-C", "genomics", "bioinformatics", ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "matplotlib", "scipy", "numba", "click", ],
+    entry_points={
+        "console_scripts": [
+            "phic = src.phic2:cli",
+        ]
+    }
+)
